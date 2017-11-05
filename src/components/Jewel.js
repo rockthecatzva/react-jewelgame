594474a390339539edc5fdfef23179f66fe1fafe
Dan Catzva <dcatzva@gmail.com>
2017-11-05 15:11:44 -0500

    jewels render with absolute positioning. basic onclick handler working to add border

--- src/components/Jewel.js
+++ src/components/Jewel.js
@@ -5,17 +5,33 @@ import styled, {} from 'styled-components';
 export default class Jewel extends Component {
     render(){
         const color = this.props.jewelType;
+        const {width, height, row, column, isSelected} = this.props;
+        const border = isSelected ? "solid 5px black": "none 0px";
+        
+
         const JewelDiv = styled.div`
+            position: absolute;
+            box-sizing: border-box;
             background-color: ${color};
             color: white;
-            height: 12%;
-            width: 100%;
+            height: ${height+"%"};
+            width: ${width+"%"};
+            top: ${(row*height)+"%"};
+            left: ${(column*width)+"%"};
+            border: ${border};
+            
             `
 
-        return (<JewelDiv>This is a jewel</JewelDiv>)
+        return (<JewelDiv onClick={()=>{this.props.onJewelClick(row, column)}}></JewelDiv>)
     }
 }
 
 Jewel.propTypes = {
     jewelType: PropTypes.string.isRequired,
+    onJewelClick: PropTypes.func.isRequired,
+    row: PropTypes.number.isRequired,
+    column: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    isSelected: PropTypes.bool.isRequired
 }
--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.initGame = this.initGame.bind(this);
-        this.state = {"jewelSet": []};
+        this.onJewelClick = this.onJewelClick.bind(this);
+        this.state = {"jewelSet": [], "selectedJewel": []};
     }
 
     initGame() {
@@ -28,30 +29,53 @@ class App extends React.Component {
                 if (1) {
                     //first col - just worry about vertical sequences
                     tempJewel = {
-                                    "jewelType": basicJewelTypes[Math.floor(Math.random() * basicJewelTypes.length)]
+                                    "jewelType": basicJewelTypes[Math.floor(Math.random() * basicJewelTypes.length)],
+                                    "row": b,
+                                    "column": a
                                 }
-                    tempSet.push(tempJewel);
+                    //tempSet.push(tempJewel);
+                    jewelSet.push(tempJewel);
                 }
             }
-            jewelSet.push(tempSet);
-            tempSet = [];
+            //jewelSet.push(tempSet);
+            //tempSet = [];
         }
 
         this.setState({"jewelSet": jewelSet});
         console.log(jewelSet);
     }
 
+    onJewelClick(row, col){
+        if(this.state.selectedJewel.length){
+            if(this.state.selectedJewel[0]!=row || this.state.selectedJewel[1]!=col){
+                //a new box has been selected
+                this.setState({"selectedJewel": [row, col]})
+            }
+        }
+        else{
+            //no prior-selected box
+            this.setState({"selectedJewel": [row, col]})
+        }
+        
+    }
+
     render() {
+        const gameDivWidth = 800,
+              gameDivHeight = 600,
+              jewelWidth = (1 / 8)*100,
+              jewelHeight = (1/8)*100;
+
         const GameDiv = styled.div`
             background-color: grey;
-            width: 800px;
-            height: 600px;
+            width: ${gameDivWidth+"px"};
+            height: ${gameDivHeight+"px"};
             margin: 0 auto;
-            
+            position: relative;
             @media (max-width: 1000px) { 
                 width: 100%;
             }`;
         
+        /*
         const FloatDiv = styled.div`
             postion: relative;
             float: left;
@@ -63,21 +87,31 @@ class App extends React.Component {
             height: 20px;
             width: 20px;
             `;    
-        
+        */
+
+        /*
         const jewelDivs = this.state.jewelSet.map((s,i)=>{
             return (<FloatDiv key={i}>{s.map((j,b)=>{
-                return (<Jewel jewelType={j.jewelType} key={b} />)
+                return (<Jewel onJewelClick={this.onJewelClick} jewelType={j.jewelType} key={b} />)
             })}</FloatDiv>)
         })
-        
-        console.log(jewelDivs)
+        */
+
+        const jewelDivs = this.state.jewelSet.map((j,i)=>{
+            let isSelected = false; 
+            if (this.state.selectedJewel.length){
+               isSelected=((j.row===this.state.selectedJewel[0])&&(j.column===this.state.selectedJewel[1]))? true: false;
+            }
+
+            return (<Jewel onJewelClick={this.onJewelClick} jewelType={j.jewelType} key={i} row={j.row} column={j.column} width={jewelWidth} height={12} isSelected={isSelected} />)})
+
+        //console.log(jewelDivs)
         
 
         return (<div>
             <h1>Simple Bejeweled Clone</h1>
             <button onClick={this.initGame}>Start/Restart</button>
-            <Test1 />
-            <Test1 primary />
+            
             <GameDiv>
                 {jewelDivs}
             </GameDiv>
